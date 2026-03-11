d1ad264fb533372c7ce38aa22172df180abeb5f3
DanielMartinez020 <dm657882@gmail.com>
2026-03-10 19:15:52 -0600

    Creación proyecto en netbeans

---
+++ src/main/java/com/EduRefuerzo/EduRefuerzoApplication.java
@@ -0,0 +1,13 @@
+package com.EduRefuerzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduRefuerzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduRefuerzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EduRefuerzo/EduRefuerzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduRefuerzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduRefuerzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

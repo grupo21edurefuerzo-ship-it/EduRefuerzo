53485671fd7eb7117a08ed95c8505922cf78e19d
Francisco Luna Bonilla <grupo21edurefuerzo@gmail.com>
2026-03-12 14:54:30 -0600

    Delete src/main/java/com/EduRefuerzo/dao/TemaDao.java

--- src/main/java/com/EduRefuerzo/dao/TemaDao.java
+++
@@ -1,8 +0,0 @@
-package com.EduRefuerzo.dao;
-
-import com.EduRefuerzo.domain.Tema;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TemaDao extends JpaRepository<Tema, Long> {
-    
-}

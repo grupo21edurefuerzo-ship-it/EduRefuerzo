ff8b79cfac772328e7dec02973dd0a3142092db3
Francisco Luna Bonilla <grupo21edurefuerzo@gmail.com>
2026-03-12 14:54:04 -0600

    Delete src/main/java/com/EduRefuerzo/dao/MateriaDao.java

--- src/main/java/com/EduRefuerzo/dao/MateriaDao.java
+++
@@ -1,8 +0,0 @@
-package com.EduRefuerzo.dao;
-
-import com.EduRefuerzo.domain.Materia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MateriaDao extends JpaRepository<Materia, Long> {
-    
-}

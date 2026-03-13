d55fc04262e02b5ada81e7da22ee2507ab4cd074
Francisco Luna Bonilla <grupo21edurefuerzo@gmail.com>
2026-03-13 10:36:14 -0600

    Add files via upload
    Implementación de interfaces DAO para el acceso a datos de materias y temas RF#4 y RF#5-Francisco Luna

---
+++ src/main/java/com/EduRefuerzo/dao/TemaDao.java
@@ -0,0 +1,7 @@
+package com.EduRefuerzo.dao;
+
+import com.EduRefuerzo.domain.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaDao extends JpaRepository<Tema, Long> {
+}
